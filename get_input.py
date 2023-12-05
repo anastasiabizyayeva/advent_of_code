4a14237ec3a2d304253d946909c5d3de07dcd251
Anastasia <anastasia.bizyayeva@gmail.com>
2023-12-05 08:31:41 +0000

    added option to not split lines

--- get_input.py
+++ get_input.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 session_cookie = os.getenv("AOC_SESSION_COOKIE")
 
-def get_advent_of_code_input(year, day_number):
+def get_advent_of_code_input(year, day_number, splitlines=True):
     url = f"https://adventofcode.com/{year}/day/{day_number}/input"
     
     cookies = {
@@ -16,6 +16,6 @@ def get_advent_of_code_input(year, day_number):
     response = requests.get(url, cookies=cookies)
     
     if response.status_code == 200:
-        return response.text.splitlines()
+        return response.text.splitlines() if splitlines else response.text
     else:
         return f"Error: Unable to fetch data, status code {response.status_code}, {response.text}"
